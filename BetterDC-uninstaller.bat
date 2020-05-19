6a6ad8c8c020ef8b7234159649735a0858b97268
Felix Seiboldt <52862870+devilAPI@users.noreply.github.com>
2020-05-19 14:03:35 +0200

    Update BetterDC-uninstaller.bat

--- BetterDC-uninstaller.bat
+++ BetterDC-uninstaller.bat
@@ -1,16 +1,16 @@
 @ECHO off
-title BetterDiscord Uninstaller by devilAPI
-:reallydelete
-echo BANDAGEDDB IS NOT SUPPORTED!
-set /P c=Thanks for downloading my uninstaller! Do you really want to uninstall BetterDiscord [Y/N]? 
+
+:prompt
+set /P c=Thanks for using my uninstaller! Are you sure you want to uninstall BetterDiscord [Y/N]? 
 echo.
-if /I "%c%" EQU "Y" goto :remove
+if /I "%c%" EQU "Y" goto :removeBetterDiscord
 if /I "%c%" EQU "N" goto :eof
-goto :reallydelete
-:remove
-echo Making Backup of BetterDiscord Folder...
-Xcopy /E /I %appdata%\BetterDiscord C:\Users\%USERNAME%\Desktop\BetterDiscord
-echo Backup successfull.
+goto :prompt
+
+:removeBetterDiscord
+echo Backing up BetterDiscord...
+echo.
+Xcopy /E /I "C:\Users\%USERNAME%\AppData\Roaming\BetterDiscord" "C:\Users\%USERNAME%\Desktop\BetterDiscord"
 echo Removing BetterDiscord...
 echo.
 call:deleteFolder %appdata%\BetterDiscord
@@ -18,23 +18,26 @@ for /d %%G in ("%localappdata%\Discord\app-*") do (
     call:deleteFolder "%%G\resources\node_modules\BetterDiscord"
     call:deleteFolder "%%G\resources\app"
 )
+
 goto:end
+
 :deleteFolder
 if exist "%~1" (
     @RD /S /Q "%~1"
     echo Folder "%~1" removed.
 ) else (
-    echo Folder "%~1" does not exist.
+    echo Folder "%~1" does not exist. This is NOT an Error. Uninstallation should still work.
 )
 goto:eof
+
 :end
 echo.
 echo Restarting Discord...
-taskkill /f /im Discord.exe 1 2
+taskkill /f /im Discord.exe 1>nul 2>nul
 "%localappdata%\Discord\Update.exe" --processStart Discord.exe
+
 echo.
-echo BetterDiscord/bandagedDB has been removed! Thanks for using my tool!
-echo Discord should now restart.
-echo if not, please 
+echo BetterDiscord has been removed!
 echo.
+
 pause
